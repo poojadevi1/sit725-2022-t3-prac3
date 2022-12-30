ff42348463a7a5bf0a983c6bb4522391ce5cebe2
poojadevi1 <119906014+poojadevi1@users.noreply.github.com>
2022-12-30 14:08:50 +1100

    updated practical 2

--- server.js
+++ server.js
@@ -11,11 +11,22 @@ const addNumbers = (number1, number2) => {
     var result = num1+num2;
     return result; 
 }
-
+const divideNumbers = (number1, number2) => {
+    var num1 = parseInt(number1)
+    var num2 = parseInt(number2)
+    var result = num1/num2;
+    return result; 
+}
 app.get("/addTwoNumbers", (req,res) => {
     var number1 = req.query.number1;
     var number2 = req.query.number2;
     var result = addNumbers(number1, number2)
+    res.json({statusCode: 200, data: result, message:'Success'})
+})
+app.get("/divideTwoNumbers", (req,res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = divideNumbers(number1, number2)
     res.json({statusCode: 200, data: result, message:'Success'})
 })
 
